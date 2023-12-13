f427ceb4007cc2e831dcf4a8fb83f2b34edf7ef8
nareshsm94 <116281653+nareshsm94@users.noreply.github.com>
2023-12-13 10:05:23 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,57 @@
+package week1.day1;
+
+public class Car {
+	
+	public static void main(String[] args) {
+		
+	
+		
+		Car obj=new Car();
+	
+		obj.soundHorn();
+		obj.applyBreak();
+		obj.applygear();
+		obj.switchonAc();
+		obj.applyAccelerate();
+		
+		System.out.println("I am Software Tester");
+	}
+	
+	public void applyBreak() {
+	
+		System.out.println("Break is applied");
+		
+	}
+	
+	
+	
+	public void soundHorn() {
+
+		System.out.println("Horn is applied");
+		
+	}
+	
+	public void applygear() {
+		
+		System.out.println("gear is applied");
+		
+	}
+	
+	public void switchonAc() {
+		
+		System.out.println("switchonAc is applied");
+		
+	}
+	
+	public void applyAccelerate() {
+		
+		System.out.println("Accelerate is applied");
+		
+	}
+	
+	
+
+	
+	
+
+}
---
+++ Mobile.java
@@ -0,0 +1,36 @@
+package week1.day1;
+
+public class Mobile {
+	public static void main(String[] args) {
+		Mobile obj=new Mobile();
+		obj.call();
+		obj.chat();
+		obj.status();
+		obj.video();
+	}
+
+
+	public void call() {
+		
+		System.out.println("call ");
+		
+	}
+	
+	public void chat() {
+		
+		System.out.println("chat ");
+		
+	}
+
+	public void status() {
+	
+	System.out.println("status ");
+	
+}
+
+	public void video() {
+	
+	System.out.println("video ");
+	
+}
+}
